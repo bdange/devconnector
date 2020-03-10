629dc2b9689e89b7134bb7870c5b7845567d6576
benjamin dange <benjamin.dange@gmail.com>
2020-03-10 22:21:38 +0100

    Trying to update profile page

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -48,6 +48,7 @@ export const getProfiles = () => async dispatch => {
 
 // Get profile by ID
 export const getProfileById = userId => async dispatch => {
+  //dispatch({ type: CLEAR_PROFILE });
   try {
     const res = await axios.get(`/api/profile/user/${userId}`);
 
@@ -224,7 +225,7 @@ export const deleteEducation = id => async dispatch => {
 export const deleteAccount = id => async dispatch => {
   if (window.confirm('Are you sure? This cannot be undone!')) {
     try {
-      const res = await axios.delete(`/api/profile`);
+      await axios.delete(`/api/profile`);
 
       dispatch({
         type: CLEAR_PROFILE
