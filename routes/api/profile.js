2c429104dddb3e5ea7c681cf3e9c9eb79ee39ebb
benjamin dange <benjamin.dange@gmail.com>
2020-03-10 21:13:27 +0100

    Fixed users display

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -117,7 +117,7 @@ router.post(
 // @access   Public
 router.get('/', async (req, res) => {
   try {
-    const profiles = await Profile.find().populate('User', ['name', 'avatar']);
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
     res.json(profiles);
   } catch (err) {
     console.error(err.message);
