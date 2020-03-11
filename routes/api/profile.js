3a656737d9cc3af30aea3c8f5242385a14b9df5a
benjamin dange <benjamin.dange@gmail.com>
2020-03-11 13:23:46 +0100

    Fixed Edit Profile issue

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -132,7 +132,7 @@ router.get('/user/:user_id', async (req, res) => {
   try {
     const profile = await Profile.findOne({
       user: req.params.user_id
-    }).populate('User', ['name', 'avatar']);
+    }).populate('user', ['name', 'avatar']);
 
     if (!profile) return res.status(400).json({ msg: 'Profile not found' });
 
