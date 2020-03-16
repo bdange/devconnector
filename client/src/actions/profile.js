c22771e32787ea626c51d5a3164523cee20281fd
benjamin dange <benjamin.dange@gmail.com>
2020-03-16 18:34:46 +0100

    Trying to display github profiles

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -48,7 +48,6 @@ export const getProfiles = () => async dispatch => {
 
 // Get profile by ID
 export const getProfileById = userId => async dispatch => {
-  //dispatch({ type: CLEAR_PROFILE });
   try {
     const res = await axios.get(`/api/profile/user/${userId}`);
 
@@ -65,7 +64,7 @@ export const getProfileById = userId => async dispatch => {
 };
 
 // Get Github repos
-export const getGitHubRepos = username => async dispatch => {
+export const getGithubRepos = username => async dispatch => {
   try {
     const res = await axios.get(`/api/profile/github/${username}`);
 
